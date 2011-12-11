9403f8f8e94521ab969b3dc3f740d1f1aab0c318
Twisted Playground <twistedumbrella@gmail.com>
2011-12-11 11:46:10 -0500

    Reconstruct Vendor
    Add better method of build selection. Import Google and Verizon vendor
    components into single repository. Add vendor call for hardware
    acceleration repository. - Shaky153 vendor repository will be used for
    testing direct hardware acceleration.

--- build_twisted.sh
+++ build_twisted.sh
@@ -4,21 +4,22 @@ export CCACHE_DIR=/Users/TwistedZero/.ccache
 prebuilt/darwin-x86/ccache/ccache -M 40G
 make clean -j8
 source build/envsetup.sh
-if [ $1 ]; then
+if [ "$1" == "mecha" ]; then
 lunch 6
-elif [ $2 ]; then
+elif [ "$1" == "ace" ]; then
 lunch 4
 fi
 export USE_CCACHE=1
 export CCACHE_DIR=/Users/TwistedZero/.ccache
 prebuilt/darwin-x86/ccache/ccache -M 40G
 make otapackage -j4
-if [ $1 ]; then
+if [ "$1" == "mecha" ]; then
 if [ -e /Volumes/android/android-tzb_ics4.0.1/out/target/product/mecha/htc_mecha-ota-eng.TwistedZero.zip ]; then
 cp -R /Volumes/android/android-tzb_ics4.0.1/out/target/product/mecha/htc_mecha-ota-eng.TwistedZero.zip /Users/TwistedZero/Dropbox/IceCreamSammy/htc_mecha-ota-eng.TwistedZero.zip
-stat -t %Y%m%d /Volumes/android/android-tzb_ics4.0.1/out/target/product/mecha/htc_mecha-ota-eng.TwistedZero.zip | cut -d\  -f11 >> /Users/TwistedZero/Dropbox/Public/BuildTimeStamp.txt
+stat -t %Y%m%d /Volumes/android/android-tzb_ics4.0.1/out/target/product/mecha/htc_mecha-ota-eng.TwistedZero.zip | cut -d\  -f11 >> /Volumes/frontrow/TwistedZero/BuildTimeStamp
 fi
-elif [ $2 ]; then
+elif ["$1" == "ace" ]; then
 if [ -e /Volumes/android/android-tzb_ics4.0.1/out/target/product/ace/htc_mecha-ota-eng.TwistedZero.zip ]; then
 cp -R /Volumes/android/android-tzb_ics4.0.1/out/target/product/ace/htc_ace-ota-eng.TwistedZero.zip /Users/TwistedZero/Dropbox/IceCreamSammy/htc_ace-ota-eng.TwistedZero.zip
+fi
 fi
--- twisted-vendor.mk
+++ twisted-vendor.mk
@@ -26,3 +26,65 @@ PRODUCT_COPY_FILES += \
     vendor/twisted/prebuilt/lib/modules/tun.ko:system/lib/modules/tun.ko \
     vendor/twisted/prebuilt/lib/modules/zram.ko:system/lib/modules/zram.ko \
     vendor/twisted/prebuilt/etc/init.d:system/etc/init.d
+
+# Google Applications
+PRODUCT_COPY_FILES += \
+    vendor/twisted/google/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk \
+    vendor/twisted/google/app/FaceLock.apk:system/app/FaceLock.apk \
+    vendor/twisted/google/app/GenieWidget.apk:system/app/GenieWidget.apk \
+    vendor/twisted/google/app/Gmail.apk:system/app/Gmail.apk \
+    vendor/twisted/google/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+    vendor/twisted/google/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+    vendor/twisted/google/app/GoogleEarth.apk:system/app/GoogleEarth.apk \
+    vendor/twisted/google/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
+    vendor/twisted/google/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+    vendor/twisted/google/app/GooglePackageVerifier.apk:system/app/GooglePackageVerifier.apk \
+    vendor/twisted/google/app/GooglePackageVerifierUpdater.apk:system/app/GooglePackageVerifierUpdater.apk \
+    vendor/twisted/google/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
+    vendor/twisted/google/app/GoogleQuickSearchBox.apk:system/app/GoogleQuickSearchBox.apk \
+    vendor/twisted/google/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+    vendor/twisted/google/app/GoogleTTS.apk:system/app/GoogleTTS.apk \
+    vendor/twisted/google/app/LatinImeGoogle.apk:system/app/LatinImeGoogle.apk \
+    vendor/twisted/google/app/Maps.apk:system/app/Maps.apk \
+    vendor/twisted/google/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
+    vendor/twisted/google/app/MediaUploader.apk:system/app/MediaUploader.apk \
+    vendor/twisted/google/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
+    vendor/twisted/google/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
+    vendor/twisted/google/app/SetupWizard.apk:system/app/SetupWizard.apk \
+    vendor/twisted/google/app/Talk.apk:system/app/Talk.apk \
+    vendor/twisted/google/app/Vending.apk:system/app/Vending.apk \
+    vendor/twisted/google/app/VoiceSearch.apk:system/app/VoiceSearch.apk \
+    vendor/twisted/google/app/YouTube.apk:system/app/YouTube.apk
+
+# Google Permissions
+PRODUCT_COPY_FILES += \
+    vendor/twisted/google/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    vendor/twisted/google/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+    vendor/twisted/google/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
+    vendor/twisted/google/etc/permissions/features.xml:system/etc/permissions/features.xml 
+
+# Google Frameworks
+PRODUCT_COPY_FILES += \
+    vendor/twisted/google/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
+    vendor/twisted/google/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+    vendor/twisted/google/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar
+
+# Google Libraries
+PRODUCT_COPY_FILES += \
+    vendor/twisted/google/lib/libfrsdk.so:system/lib/libfrsdk.so \
+    vendor/twisted/google/lib/libvoicesearch.so:system/lib/libvoicesearch.so \
+    vendor/twisted/google/lib/libfacelock_jni.so:system/lib/libfacelock_jni.so \
+    vendor/twisted/google/lib/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so
+
+# Google Vendor
+PRODUCT_COPY_FILES += \
+    vendor/twisted/google/vendor/pittpatt:system/vendor/pittpatt
+
+# Verizon Applications
+PRODUCT_COPY_FILES += \
+    vendor/twisted/verizon/app/VZWAPNLib.apk:system/app/VZWAPNLib.apk \
+    vendor/twisted/verizon/app/VZWAPNService.apk:system/app/VZWAPNService.apk
+
+# Verizon Customization
+PRODUCT_COPY_FILES += \
+    vendor/twisted/verizon/media/bootanimation.zip:system/media/bootanimation.zip
