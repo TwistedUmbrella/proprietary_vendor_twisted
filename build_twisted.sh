e3104da4cf2b04d4e9852a2af158a5c1ef33b3f4
Twisted Playground <twistedumbrella@gmail.com>
2011-12-13 01:13:47 -0500

    Status Output Messages
    Design a method of status notification that will update the timestamp
    file for new build processes, successful builds, and failed builds.
    Most recent successful build should also be listed with any new or
    failed build notifications.

--- build_twisted.sh
+++ build_twisted.sh
@@ -4,6 +4,10 @@ read config
 cd /Volumes/android/android-tzb_ics4.0.1/kernel/leanKernel-tbolt-ics
 ./buildlean.sh 1 $config
 fi
+if [ "$1" == "mecha" ]; then
+echo "Build Notes: "
+read changes
+fi
 cd /Volumes/android/android-tzb_ics4.0.1
 export USE_CCACHE=1
 export CCACHE_DIR=/Users/TwistedZero/.ccache
@@ -12,6 +16,14 @@ make clean -j8
 source build/envsetup.sh
 if [ "$1" == "mecha" ]; then
 lunch 6
+echo "" >> /Volumes/frontrow/TwistedZero/TimeStamp
+echo "New Compile Started:" > /Volumes/frontrow/TwistedZero/TimeStamp
+date >> /Volumes/frontrow/TwistedZero/TimeStamp
+echo "Compile Information:" >> /Volumes/frontrow/TwistedZero/TimeStamp
+echo $changes >> /Volumes/frontrow/TwistedZero/TimeStamp
+cat /Volumes/frontrow/TwistedZero/BackStamp /Volumes/frontrow/TwistedZero/TimeStamp > /Volumes/frontrow/TwistedZero/TempStamp
+mv -f /Volumes/frontrow/TwistedZero/TempStamp /Volumes/frontrow/TwistedZero/TimeStamp
+rm -R /Volumes/frontrow/TwistedZero/TempStamp
 elif [ "$1" == "ace" ]; then
 lunch 4
 fi
@@ -24,7 +36,14 @@ if [ -e /Volumes/android/android-tzb_ics4.0.1/out/target/product/mecha/htc_mecha
 cp -R /Volumes/android/android-tzb_ics4.0.1/out/target/product/mecha/htc_mecha-ota-eng.TwistedZero.zip /Users/TwistedZero/Dropbox/IceCreamSammy/htc_mecha-ota-eng.TwistedZero.zip
 echo "Latest Build Completed:" > /Volumes/frontrow/TwistedZero/TimeStamp
 date >> /Volumes/frontrow/TwistedZero/TimeStamp
-echo "Please Allow 30-45 Min" >> /Volumes/frontrow/TwistedZero/TimeStamp
+echo "Please Allow 30-45 Min." >> /Volumes/frontrow/TwistedZero/TimeStamp
+cp -R /Volumes/frontrow/TwistedZero/TimeStamp /Volumes/frontrow/TwistedZero/BackStamp
+else
+echo "Compile Process Failed." > /Volumes/frontrow/TwistedZero/TimeStamp
+echo "" >> /Volumes/frontrow/TwistedZero/TimeStamp
+cat /Volumes/frontrow/TwistedZero/TimeStamp /Volumes/frontrow/TwistedZero/BackStamp > /Volumes/frontrow/TwistedZero/TempStamp
+mv -f /Volumes/frontrow/TwistedZero/TempStamp /Volumes/frontrow/TwistedZero/TimeStamp
+rm -R /Volumes/frontrow/TwistedZero/TempStamp
 fi
 elif ["$1" == "ace" ]; then
 if [ -e /Volumes/android/android-tzb_ics4.0.1/out/target/product/ace/htc_mecha-ota-eng.TwistedZero.zip ]; then
