1e9aaa41d9501b085224bdbde67203115a570c8b
Twisted Playground <twistedumbrella@gmail.com>
2011-12-19 01:14:00 -0500

    Less Static, More Variable

--- build_twisted.sh
+++ build_twisted.sh
@@ -1,34 +1,44 @@
+TIMESTAMP=/Volumes/frontrow/TwistedZero/TimeStamp
+TEMPSTAMP=/Volumes/frontrow/TwistedZero/TempStamp
+BACKSTAMP=/Volumes/frontrow/TwistedZero/BackStamp
+BUILDDIR=/Volumes/android/android-tzb_ics4.0.1
+CCACHEDIR=prebuilt/darwin-x86/ccache/ccache
+KERNELSPEC=leanKernel-tbolt-ics
+USERLOCAL=/Users/TwistedZero
+DROPBOX=/Users/TwistedZero/Dropbox/IceCreamSammy
+HANDLE=TwistedZero
+
 if [ "$1" == "mecha" ]; then
 echo "Build Notes: "
 read changes
-echo "" > /Volumes/frontrow/TwistedZero/TimeStamp
-echo "New Compile Started:" >> /Volumes/frontrow/TwistedZero/TimeStamp
-date >> /Volumes/frontrow/TwistedZero/TimeStamp
-echo "Compile Information:" >> /Volumes/frontrow/TwistedZero/TimeStamp
-echo $changes >> /Volumes/frontrow/TwistedZero/TimeStamp
-cat /Volumes/frontrow/TwistedZero/BackStamp /Volumes/frontrow/TwistedZero/TimeStamp > /Volumes/frontrow/TwistedZero/TempStamp
-mv -f /Volumes/frontrow/TwistedZero/TempStamp /Volumes/frontrow/TwistedZero/TimeStamp
+echo "" > $TIMESTAMP
+echo "New Compile Started:" >> $TIMESTAMP
+date >> $TIMESTAMP
+echo "Compile Information:" >> $TIMESTAMP
+echo $changes >> $TIMESTAMP
+cat $BACKSTAMP $TIMESTAMP > $TEMPSTAMP
+mv -f $TEMPSTAMP $TIMESTAMP
 if [ "$2" == "Y" ]; then
 echo "Config Name? ";
-cd /Volumes/android/android-tzb_ics4.0.1/kernel/leanKernel-tbolt-ics
+cd $BUILDDIR/kernel/$KERNELSPEC
 ls config
 read config
 ./buildlean.sh 1 $config
 if [ -e arch/arm/boot/zImage ]; then
-echo "" >> /Volumes/frontrow/TwistedZero/TimeStamp
-echo "Kernel Compile Success." >> /Volumes/frontrow/TwistedZero/TimeStamp
-echo "" >> /Volumes/frontrow/TwistedZero/TimeStamp
+echo "" >> $TIMESTAMP
+echo "Kernel Compile Success." >> $TIMESTAMP
+echo "" >> $TIMESTAMP
 else
-echo "" >> /Volumes/frontrow/TwistedZero/TimeStamp
-echo "-Kernel Compile Failed." >> /Volumes/frontrow/TwistedZero/TimeStamp
-echo "" >> /Volumes/frontrow/TwistedZero/TimeStamp
+echo "" >> $TIMESTAMP
+echo "-Kernel Compile Failed." >> $TIMESTAMP
+echo "" >> $TIMESTAMP
 fi
 fi
 fi
-cd /Volumes/android/android-tzb_ics4.0.1
+cd $BUILDDIR
 export USE_CCACHE=1
-export CCACHE_DIR=/Users/TwistedZero/.ccache
-prebuilt/darwin-x86/ccache/ccache -M 40G
+export CCACHE_DIR=$USERLOCAL/.ccache
+$CCACHEDIR -M 40G
 make clean -j8
 source build/envsetup.sh
 if [ "$1" == "mecha" ]; then
@@ -37,24 +47,24 @@ elif [ "$1" == "ace" ]; then
 lunch 4
 fi
 export USE_CCACHE=1
-export CCACHE_DIR=/Users/TwistedZero/.ccache
-prebuilt/darwin-x86/ccache/ccache -M 40G
+export CCACHE_DIR=$USERLOCAL/.ccache
+$CCACHEDIR -M 40G
 make otapackage -j4
 if [ "$1" == "mecha" ]; then
-if [ -e /Volumes/android/android-tzb_ics4.0.1/out/target/product/mecha/htc_mecha-ota-eng.TwistedZero.zip ]; then
-cp -R /Volumes/android/android-tzb_ics4.0.1/out/target/product/mecha/htc_mecha-ota-eng.TwistedZero.zip /Users/TwistedZero/Dropbox/IceCreamSammy/htc_mecha-ota-eng.TwistedZero.zip
-echo "Latest Build Completed:" > /Volumes/frontrow/TwistedZero/TimeStamp
-date >> /Volumes/frontrow/TwistedZero/TimeStamp
-echo "Please Allow 30-45 Min." >> /Volumes/frontrow/TwistedZero/TimeStamp
-cp -R /Volumes/frontrow/TwistedZero/TimeStamp /Volumes/frontrow/TwistedZero/BackStamp
+if [ -e $BUILDDIR/out/target/product/mecha/htc_mecha-ota-eng.TwistedZero.zip ]; then
+cp -R $BUILDDIR/out/target/product/mecha/htc_mecha-ota-eng.$HANDLE.zip $DROPBOX/htc_mecha-ota-eng.$HANDLE.zip
+echo "Latest Build Completed:" > $TIMESTAMP
+date >> $TIMESTAMP
+echo "Please Allow 30-45 Min." >> $TIMESTAMP
+cp -R $TIMESTAMP $BACKSTAMP
 else
-echo "Compile Process Failed." > /Volumes/frontrow/TwistedZero/TimeStamp
-echo "" >> /Volumes/frontrow/TwistedZero/TimeStamp
-cat /Volumes/frontrow/TwistedZero/TimeStamp /Volumes/frontrow/TwistedZero/BackStamp > /Volumes/frontrow/TwistedZero/TempStamp
-mv -f /Volumes/frontrow/TwistedZero/TempStamp /Volumes/frontrow/TwistedZero/TimeStamp
+echo "Compile Process Failed." > $TIMESTAMP
+echo "" >> $TIMESTAMP
+cat $TIMESTAMP $BACKSTAMP > $TEMPSTAMP
+mv -f $TEMPSTAMP $TIMESTAMP
 fi
 elif ["$1" == "ace" ]; then
-if [ -e /Volumes/android/android-tzb_ics4.0.1/out/target/product/ace/htc_mecha-ota-eng.TwistedZero.zip ]; then
-cp -R /Volumes/android/android-tzb_ics4.0.1/out/target/product/ace/htc_ace-ota-eng.TwistedZero.zip /Users/TwistedZero/Dropbox/IceCreamSammy/htc_ace-ota-eng.TwistedZero.zip
+if [ -e $BUILDDIR/out/target/product/ace/htc_mecha-ota-eng.$HANDLE.zip ]; then
+cp -R $BUILDDIR/out/target/product/ace/htc_ace-ota-eng.TwistedZero.zip $DROPBOX/htc_ace-ota-eng.TwistedZero.zip
 fi
 fi
