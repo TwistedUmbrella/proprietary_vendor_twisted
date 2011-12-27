cc97391d0fc6a6495ea680bd5de54854f57b2617
Twisted Playground <twistedumbrella@gmail.com>
2011-12-27 17:23:59 -0500

    Improve Variable Declaration Scope

--- build_twisted.sh
+++ build_twisted.sh
@@ -1,12 +1,12 @@
-TIMESTAMP=/Volumes/frontrow/TwistedZero/TimeStamp
-TEMPSTAMP=/Volumes/frontrow/TwistedZero/TempStamp
-BACKSTAMP=/Volumes/frontrow/TwistedZero/BackStamp
+HANDLE=TwistedZero
+TIMESTAMP=/Volumes/frontrow/$HANDLE/TimeStamp
+TEMPSTAMP=/Volumes/frontrow/$HANDLE/TempStamp
+BACKSTAMP=/Volumes/frontrow/$HANDLE/BackStamp
 BUILDDIR=/Volumes/android/android-tzb_ics4.0.1
 CCACHEBIN=prebuilt/darwin-x86/ccache/ccache
 KERNELSPEC=leanKernel-tbolt-ics
-USERLOCAL=/Users/TwistedZero
-DROPBOX=/Users/TwistedZero/Dropbox/IceCreamSammy
-HANDLE=TwistedZero
+USERLOCAL=/Users/$HANDLE
+DROPBOX=/Users/$HANDLE/Dropbox/IceCreamSammy
 
 if [ "$1" == "mecha" ]; then
 echo "Build Notes: "
@@ -57,7 +57,7 @@ $CCACHEBIN -M 40G
 make otapackage -j4
 rm -R $CCACHE_DIR/*
 if [ "$1" == "mecha" ]; then
-if [ -e $BUILDDIR/out/target/product/mecha/htc_mecha-ota-eng.TwistedZero.zip ]; then
+if [ -e $BUILDDIR/out/target/product/mecha/htc_mecha-ota-eng.$HANDLE.zip ]; then
 cp -R $BUILDDIR/out/target/product/mecha/htc_mecha-ota-eng.$HANDLE.zip $DROPBOX/htc_mecha-ota-eng.$HANDLE.zip
 echo "Latest Build Completed:" > $TIMESTAMP
 date >> $TIMESTAMP
@@ -71,6 +71,6 @@ mv -f $TEMPSTAMP $TIMESTAMP
 fi
 elif ["$1" == "ace" ]; then
 if [ -e $BUILDDIR/out/target/product/ace/htc_mecha-ota-eng.$HANDLE.zip ]; then
-cp -R $BUILDDIR/out/target/product/ace/htc_ace-ota-eng.TwistedZero.zip $DROPBOX/htc_ace-ota-eng.TwistedZero.zip
+cp -R $BUILDDIR/out/target/product/ace/htc_ace-ota-eng.$HANDLE.zip $DROPBOX/htc_ace-ota-eng.$HANDLE.zip
 fi
 fi
