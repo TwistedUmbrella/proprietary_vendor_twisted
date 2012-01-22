582148af7f4eea133c3e87507bfa46f66cde9ff9
Twisted <twistedumbrella@gmail.com>
2012-01-22 14:52:44 -0500

    Device-Specific Output Info Titles - Easier Identification

--- build_twisted.sh
+++ build_twisted.sh
@@ -60,6 +60,7 @@ specDevice() {
         echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">' >> $TIMESTAMP
         echo '<!-- Mobile IE needs ClearType for smoothing fonts -->' >> $TIMESTAMP
         echo '<meta http-equiv="cleartype" content="on">' >> $TIMESTAMP
+        echo '<title>'${PROPER}' TimeStamp - Playground ICS</title>' >> $TIMESTAMP
         echo '</head>'>> $TIMESTAMP
         echo '<body>' >> $TIMESTAMP
         echo '<center>' >> $TIMESTAMP
